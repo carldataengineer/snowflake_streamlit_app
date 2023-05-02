463466fd6b320732f815aabd396ff07fe4859c35
carldataengineer <132379430+carldataengineer@users.noreply.github.com>
2023-05-02 17:08:30 +0100

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My New Healthy Dinner')
+
+streamlit.header('Menu')
+
+streamlit.text('Loverly Greens')
+streamlit.text('Loverly Blues')
+streamlit.text('Loverly Reds')
